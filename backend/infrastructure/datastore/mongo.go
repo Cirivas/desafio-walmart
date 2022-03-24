b09330af853c3b6c4dbff48dcfc744e2f54d81b7
Camilo Rivas <crivas.folch@gmail.com>
2022-03-24 19:58:55 -0300

    use cancel in context

--- backend/infrastructure/datastore/mongo.go
+++ backend/infrastructure/datastore/mongo.go
@@ -28,7 +28,9 @@ func NewDB() *DbConnection {
 	}
 
 	// Connect client with context
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatalln(err)
