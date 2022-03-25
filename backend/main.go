d196e37a6ee6e15acaf6b69b6dfa5c315b9b75bf
Camilo Rivas <crivas.folch@gmail.com>
2022-03-25 12:42:09 -0300

    add API booting

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/Cirivas/desafio-walmart/infrastructure/datastore"
+	"github.com/Cirivas/desafio-walmart/infrastructure/router"
+	"github.com/Cirivas/desafio-walmart/registry"
+	gorillaHandlers "github.com/gorilla/handlers"
 )
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	db := datastore.NewDB()
+	defer db.Close()
+
+	r := registry.NewRegistry(db)
+
+	api := router.NewRouter(r.NewMainController())
+
+	headers := gorillaHandlers.AllowedHeaders([]string{"Contety-Type", "Access-Control-Allow-Headers", "Authorization"})
+	methods := gorillaHandlers.AllowedMethods([]string{"DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"})
+	origins := gorillaHandlers.AllowedOrigins([]string{"*"})
+
+	log.Fatal(http.ListenAndServe(":8080", gorillaHandlers.CORS(headers, methods, origins)(api.Router())))
 }
