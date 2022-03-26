e6cf578575965a347696da68a8949e3f1c7602ee
Camilo Rivas <crivas.folch@gmail.com>
2022-03-26 18:14:27 -0300

    apply linter to file

--- frontend/src/usecase/interactor/products.interactor.test.tsx
+++ frontend/src/usecase/interactor/products.interactor.test.tsx
@@ -1,5 +1,5 @@
-jest.mock("../repository/mock/products.repository.mock")
-jest.mock("../presenter/mock/product.presenter.mock")
+jest.mock("../repository/mock/products.repository.mock");
+jest.mock("../presenter/mock/product.presenter.mock");
 
 import { Product } from "../../domain/product";
 import productPresenterMock from "../presenter/mock/product.presenter.mock";
@@ -7,36 +7,41 @@ import productsRepositoryMock from "../repository/mock/products.repository.mock"
 import { newProductInteractor } from "./products.interactor";
 
 describe("getting products", () => {
-    it("should get all products", (done) => {
-        const productsRepository = jest.mocked(productsRepositoryMock, true);
-        const productsPresenter = jest.mocked(productPresenterMock, true);
+  it("should get all products", (done) => {
+    const productsRepository = jest.mocked(productsRepositoryMock, true);
+    const productsPresenter = jest.mocked(productPresenterMock, true);
 
-        const expectedProducts: Product[] = [{
-            id: 1,
-            brand: "brand",
-            description: "description",
-            image: "imagelink",
-            price: 5,
-        }];
+    const expectedProducts: Product[] = [
+      {
+        id: 1,
+        brand: "brand",
+        description: "description",
+        image: "imagelink",
+        price: 5,
+      },
+    ];
 
-        productsRepository
-            .findAll
-            .mockImplementation(jest.fn(() => Promise.resolve(expectedProducts)))
+    productsRepository.findAll.mockImplementation(
+      jest.fn(() => Promise.resolve(expectedProducts))
+    );
 
-        productsPresenter
-            .responseProducts
-            .mockImplementation(jest.fn((p: Product[]) => p)
-        )
-        
+    productsPresenter.responseProducts.mockImplementation(
+      jest.fn((p: Product[]) => p)
+    );
 
-        const interactor = newProductInteractor(productsRepository, productsPresenter);
+    const interactor = newProductInteractor(
+      productsRepository,
+      productsPresenter
+    );
 
-        interactor.getAll().then(results => {
-            expect.assertions(2);
-            expect(results).toEqual(expectedProducts);
-            expect(productsPresenter.responseProducts.mock?.calls?.length).toBe(1);
-            done()
-        }).catch(done);
-    })
-})
-
+    interactor
+      .getAll()
+      .then((results) => {
+        expect.assertions(2);
+        expect(results).toEqual(expectedProducts);
+        expect(productsPresenter.responseProducts.mock?.calls?.length).toBe(1);
+        done();
+      })
+      .catch(done);
+  });
+});
