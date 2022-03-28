50d8dafb55311a95f75979698b4884e436ecfba5
Camilo Rivas <crivas.folch@gmail.com>
2022-03-28 04:05:40 -0300

    add dscounts to main controller

--- frontend/src/adapters/main.controller.ts
+++ frontend/src/adapters/main.controller.ts
@@ -1,12 +1,15 @@
+import { DiscountsController } from "./discounts/discounts.controller";
 import { ProductsController } from "./products/products.controller";
 import { ShoppingCartController } from "./shoppingCart/shoppingCart.controller";
 
 export interface MainController {
   productsController: ProductsController;
   shoppingCartController: ShoppingCartController;
+  discountsController: DiscountsController;
 }
 
 export default {
   productsController: {},
   shoppingCartController: {},
+  discountsController: {},
 } as MainController;
