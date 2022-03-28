7249ce2fd46dd0e2713caf4beb25cb2304aec4b5
Camilo Rivas <crivas.folch@gmail.com>
2022-03-28 04:03:59 -0300

    delete order from domain

--- frontend/src/domain/order.ts
+++
@@ -1,8 +0,0 @@
-import { Discount } from "./discount";
-import { ShoppingCart } from "./shopppingCart";
-
-export type Order = {
-    shoppingCart: ShoppingCart;
-    total:  number;
-    discountUsed: Discount;
-}
